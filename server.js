9765da2507220a9f314ffb9e8c038dbb96ddec68
johnahnz0rs <johnahn123@gmail.com>
2018-10-08 12:52:09 -0700

    pushing to heroku

--- server.js
+++ server.js
@@ -80,17 +80,17 @@ app.get('*', function(req, res) {
 });
 
 
+//
+// // production deployment to heroku
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+// }
 
-
-// production deployment to heroku
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
 
 
 
