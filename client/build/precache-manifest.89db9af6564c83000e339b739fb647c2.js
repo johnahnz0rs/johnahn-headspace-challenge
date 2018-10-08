eb94985c40c2552c62c8ed0109d1f336de31a7a4
johnahnz0rs <johnahn123@gmail.com>
2018-10-08 12:29:58 -0700

    pushing to heroku

---
+++ client/build/precache-manifest.89db9af6564c83000e339b739fb647c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdd3949fbf8f333d23b2",
+    "url": "/static/js/main.cdd3949f.chunk.js"
+  },
+  {
+    "revision": "af5e5348103b55f5500b",
+    "url": "/static/js/1.af5e5348.chunk.js"
+  },
+  {
+    "revision": "cdd3949fbf8f333d23b2",
+    "url": "/static/css/main.08c5eda0.chunk.css"
+  },
+  {
+    "revision": "f7e5c59fb9a9b9fb0c2561f892d833e9",
+    "url": "/index.html"
+  }
+];
